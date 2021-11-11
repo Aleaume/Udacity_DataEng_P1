2898d5808c4484ef734212f427ddf9beccc99e19
Aléaume <32632731+Aleaume@users.noreply.github.com>
2021-11-11 13:58:12 +0100

    Added NOT NULL constraint + cleaned up & ident INSERTS

--- sql_queries.py
+++ sql_queries.py
@@ -10,11 +10,11 @@ time_table_drop = "DROP table IF EXISTS time"
 
 songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays(\
                         songplay_id SERIAL PRIMARY KEY, \
-                        start_time bigint, \
-                        user_id int,\
+                        start_time bigint NOT NULL, \
+                        user_id int NOT NULL,\
                         level varchar,\
-                        song_id varchar,\
-                        artist_id varchar,\
+                        song_id varchar NOT NULL,\
+                        artist_id varchar NOT NULL,\
                         session_id int,\
                         location varchar,\
                         user_agent varchar);
@@ -31,7 +31,7 @@ user_table_create = ("""CREATE TABLE IF NOT EXISTS users(\
 song_table_create = ("""CREATE TABLE IF NOT EXISTS songs(\
                     song_id varchar PRIMARY KEY,\
                     title varchar,\
-                    artist_id varchar,\
+                    artist_id varchar NOT NULL,\
                     year int,\
                     duration float);
 """)
@@ -62,27 +62,37 @@ songplay_table_insert = ("""INSERT INTO songplays(start_time, user_id,level, son
 """)
 
 user_table_insert = ("""INSERT INTO users(user_id, first_name, last_name, gender, level)\
-                    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;
+                    VALUES (%s, %s, %s, %s, %s) \
+                    ON CONFLICT (user_id) \
+                    DO NOTHING;
 """)
 
 song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration)\
-                    VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;
+                    VALUES (%s, %s, %s, %s, %s) \
+                    ON CONFLICT (song_id) \
+                    DO NOTHING;
 """)
 
 artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude)\
-                        VALUES (%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING ;
+                        VALUES (%s, %s, %s, %s, %s) \
+                        ON CONFLICT (artist_id) \
+                        DO NOTHING ;
 """)
 
 
 time_table_insert = ("""INSERT INTO time(start_time, hour, day, week, month, year, weekday)\
-                    VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;
+                    VALUES (%s, %s, %s, %s, %s, %s, %s) \
+                    ON CONFLICT (start_time) \
+                    DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""SELECT song_id , artists.artist_id \
                 FROM (songs JOIN artists ON songs.artist_id = artists.artist_id) \
-                WHERE songs.title = (%s) AND  artists.name= (%s) AND songs.duration = (%s) ;
+                WHERE songs.title = (%s) \
+                AND  artists.name= (%s) \
+                AND songs.duration = (%s) ;
 """)
 
 # QUERY LISTS
